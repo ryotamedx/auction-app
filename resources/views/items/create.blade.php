d82bd011df4cf9d009e0c46036564ad9cc4c54c4
ryotamedy <ryotamex2@gmail.com>
2021-09-19 13:20:30 +0900

    バリデーション実装

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -8,32 +8,44 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
+    @if ($errors->any())
+        <div class="error">
+            <p>
+                <b>{{ count($errors) }}件のエラーがあります。</b>
+            </p>
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <h1>商品登録フォーム</h1>
     <form action="/items" method="post">
         @csrf
         <p>
             <label for="name">商品名</label>
-            <input type="text" name="name" id="">
+            <input type="text" name="name" id="" value="{{ old('name') }}">
         </p>
         <p>
             <label for="description">商品詳細</label>
-            <input type="text" name="description" id="">
+            <input type="text" name="description" id="" value="{{ old('description') }}">
         </p>
         <p>
             <label for="price">価格</label>
-            <input type="text" name="price" id="">
+            <input type="text" name="price" id="" value="{{ old('price') }}">
         </p>
         <p>
             <label for="seller">出品者</label>
-            <input type="text" name="seller" id="">
+            <input type="text" name="seller" id="" value="{{ old('seller') }}">
         </p>
         <p>
             <label for="email">電子メール</label>
-            <input type="text" name="email" id="">
+            <input type="text" name="email" id="" value="{{ old('email') }}">
         </p>
         <p>
             <label for="image_url">商品画像URL</label>
-            <input type="text" name="image_url" id="">
+            <input type="text" name="image_url" id="" value="{{ old('image_url') }}">
         </p>
 
         <input type="submit" value="登録">
