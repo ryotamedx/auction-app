b5bea346c132aa6b8a81a4b82e6c85d56f5e52c0
ryotamedy <ryotamex2@gmail.com>
2021-09-19 13:28:26 +0900

    Herokuデプロイのための設定

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\ItemController::class, 'index']);
 
 Route::resource('items', App\Http\Controllers\ItemController::class);
